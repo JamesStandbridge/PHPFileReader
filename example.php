306c8fef2fa4194ac873b5f78d9af31851186e4c
jamesstandbridge <james.standbridge.git@gmail.com>
2021-09-06 17:03:34 +0200

    feat: CSV reader && CSV reader firstline header

---
+++ example.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+
+use PHPFileReader\Tool\FileReader;
+
+$FILE_PATHNAME = __DIR__."/testData/export_clients.csv";
+
+$file = FileReader::CSV_ARRAY($FILE_PATHNAME, "|", false);
+var_dump($file);
--- src/Tool/FileReader.php
+++ src/Tool/FileReader.php
@@ -3,10 +3,44 @@
 namespace PHPFileReader\Tool;
 
 class FileReader {
+    public static function CSV_ARRAY(string $path, $delimiter = ",", bool $first_line_is_headers = false)
+    {
+        try {
+            if (!file_exists($path) || !is_readable($path)) {
+                return FALSE;
+            }      
+            $data = array();
+        	$headers = array();
+        	$index = 0;
 
+            if (($handle = fopen($path, "r")) !== FALSE) {
+                while ($row = fgetcsv($handle, 1000, $delimiter)) {
+                	//headers builder
+                	if($index === 0 && $first_line_is_headers) {
+                		if($first_line_is_headers) {
+                			foreach($row as $key => $header) 
+                				$headers[$key] = $header;
+                		}
+                	} else {
+	                 	$line = $row;
 
-	public static function CSV_ARRAY(string $pathname) : array
-	{
-		
-	}
+	                	if($first_line_is_headers) {
+		                	$line = array();
+		                	foreach($row as $key => $slug) {
+		                		$line[$headers[$key]] = $slug;
+		                	}                		
+	                	}
+
+	                    $data[] = $line;               		
+                	}
+
+                    $index++;
+                }
+                fclose($handle);
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+        return $data;
+    }
 }
