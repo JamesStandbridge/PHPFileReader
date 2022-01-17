e589c9937048ce799453bc3c1f3b3fe385a16e17
jamesstandbridge <james.standbridge.git@gmail.com>
2022-01-17 11:13:57 +0100

    uu

--- example.php
+++ example.php
@@ -3,10 +3,10 @@
 require 'vendor/autoload.php';
 
 
-
 use PHPFileReader\Tool\FileReader;
 
-$FILE_PATHNAME = __DIR__."/testData/export_clients.csv";
+
+/*$FILE_PATHNAME = __DIR__."/testData/export_clients.csv";
 
 $file = FileReader::CSV_ARRAY($FILE_PATHNAME, "|", false);
-var_dump($file);
+var_dump($file);*/
